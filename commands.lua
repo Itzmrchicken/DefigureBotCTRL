53464b2214f8ecc37c703db1d0ce47c5950aae83
ltzmrchicken <130897982+Itzmrchicken@users.noreply.github.com>
2025-05-20 21:24:55 -0400

    Update commands.lua

--- commands.lua
+++ commands.lua
@@ -82,7 +82,7 @@ return function(Msg, functions)
 	local Split = Msg:split(" ")
 	local Cmd = Split[1]:lower()
 
-	functions.BotChat(1, "/w "..Master.." running "..Cmd)
+	functions.BotChat(1, "Running "..Cmd)
 	
 	if Cmd == Prefix.."chat" then
 		local ChatMsg = Msg:gsub(Cmd, "")
